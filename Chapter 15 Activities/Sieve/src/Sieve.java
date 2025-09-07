a1922a3c5e9b3572c94638b38c589147b1ff42a3
katherinedillon <kedillon@stu.naperville203.org>
2025-09-07 11:24:13 -0500

    9/7

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,25 +1,40 @@
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
 
 /**
  * A program that implements the sieve of Eratosthenes.
 */
-public class Sieve
-{
-    public static void main(String[] args)
-    {
+public class Sieve {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Compute primes up to which integer?");
+        System.out.println("Compute primes up to which integer? ");
         int n = in.nextInt();
 
         // Your work goes here
-        . . .
+        Set<Integer> listNums = new HashSet<>();
 
+        for (int i = 2; i <= n; i++) {
+            listNums.add(i);
+        }
 
+        int count = 2;
 
+        while (count <= n) {
+            Iterator<Integer> iterator = listNums.iterator();
+            while (iterator.hasNext()) {
+                int value = iterator.next();
+                if ((value % count == 0) && (value > count))  {
+                    iterator.remove();
+                    //System.out.println(value);
+                }
 
+            }
+            count++;
 
-
-
+        }
+        System.out.print(listNums);
     }
 }
