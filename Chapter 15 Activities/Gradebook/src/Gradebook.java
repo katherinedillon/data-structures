a98781ed3b33989a009139d0a2e86cfe3c6ac193
katherinedillon <kedillon@stu.naperville203.org>
2025-09-11 13:09:31 -0500

    9/11

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-. . .
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,29 +12,48 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
-
+        Map <String, String> listStudents = new HashMap<>();
         boolean done = false;
         while(!done)
         {
             System.out.println("A)dd R)emove M)odify P)rint Q)uit");
             String input = in.next().toUpperCase();
+            
+
             if (input.equals("Q"))
             {
                 done = true;
             } else if (input.equals("A"))
             {
-                . . .
+                System.out.println("Name?");
+                String name = in.next().toUpperCase();
+
+                System.out.println("Grade?");
+                String grade = in.next().toUpperCase();
+
+                listStudents.put(name,grade);
+
 
             } else if (input.equals("R"))
             {
-                . . .
+                System.out.println("Name?");
+                String name = in.next().toUpperCase();
+
+                listStudents.remove(name);
+
             } else if (input.equals("M"))
             {
-                . . .
+                System.out.println("Name?");
+                String name = in.next().toUpperCase();
+
+                System.out.println("Grade?");
+                String grade = in.next().toUpperCase();
+
+                listStudents.put(name,grade);
+                
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                System.out.println(listStudents);
             } else
             {
                 done = true;
