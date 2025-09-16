2cafad3394e12baac527a6e355fa04f7c63b0cee
katherinedillon <kedillon@stu.naperville203.org>
2025-09-16 13:20:40 -0500

    9/16 notes

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,38 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        // Create a priority queue of strings
+        // A priority queue needs to be composed of Comparable objects
+        Queue<String> students = new PriorityQueue<>();
+
+        students.add("Neel");
+        students.add("Jonathan");
+        students.add("Cam");
+        students.add("Kaitlyn");
+        students.add("Dylan");
+
+        // The next highest priority object is moved to the front of the queue when the head of the queue is removed
+        while (students.size() > 0){
+            System.out.println("Printing: "+students.remove());
+        }
+
+        // Create a todo list
+        // The WorkOrder class has a priority and description
+        Queue<WorkOrder> toDo = new PriorityQueue<>();
+
+        //Lower priority = more important
+        toDo.add(new WorkOrder(3, "Water Plants"));
+        toDo.add(new WorkOrder(2, "Make Dinner"));
+        toDo.add(new WorkOrder(1, "Conquer World"));
+        toDo.add(new WorkOrder(9, "Video Games"));
+        toDo.add(new WorkOrder(1, "Study for Test"));
+
+        // Onjects are NOT stored in priority order
+        System.out.println(toDo);
+
+        // However, objects are removed in priority order
+        while (toDo.size() > 0){
+            System.out.println(toDo.remove());
+        }
     }
 }
